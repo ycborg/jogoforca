dbb502c8f6233680651b3bac4b18dce46d0f6cbc
Ycaro Gabriell Borges <ycarogborges@gmail.com>
2023-05-20 14:25:08 -0300

    arrumei alguns bugs e limpei o código

--- forca.c
+++ forca.c
@@ -6,21 +6,19 @@
 
 #define MAX_ERROS 6
 
-int desenho(int erros) {
-    if (erros == 0) {
-        printf(" ____\n|    |\n|   \n|  \n|    \n|\n|\n");
-    } else if (erros == 1) {
-        printf(" ____\n|    |\n|    O\n|  \n|    \n|\n|\n");
-    } else if (erros == 2) {
-        printf(" ____\n|    |\n|    O\n|    |\n|    \n|\n|\n");
-    } else if (erros == 3) {
+void desenho(int erros) {
+    if (erros == 0)
+        printf(" ____\n|    |\n|\n|\n|\n|\n|\n");
+    else if (erros == 1)
+        printf(" ____\n|    |\n|    O\n|\n|\n|\n|\n");
+    else if (erros == 2)
+        printf(" ____\n|    |\n|    O\n|    |\n|\n|\n|\n");
+    else if (erros == 3)
         printf(" ____\n|    |\n|    O\n|    |\n|   /\n|\n|\n");
-    } else if (erros == 4) {
+    else if (erros == 4)
         printf(" ____\n|    |\n|    O\n|    |\n|   / \\\n|\n|\n");
-    } else {
+    else
         printf(" ____\n|    |\n|    O\n|   /|\n|   / \\\n|\n|\n");
-    }
-    return 0;
 }
 
 int main(int argc, char *argv[0]) {
@@ -29,7 +27,7 @@ int main(int argc, char *argv[0]) {
 
     if (argc != 2) {
         printf("Digite: %s <nomedoarquivo>\n", argv[0]);
-    return 1;
+        return 1;
     }
 
     char *filename = argv[1];
@@ -37,14 +35,14 @@ int main(int argc, char *argv[0]) {
 
     if (!fp) {
         printf("Erro: não consigo abrir esse arquivo %s\n", filename);
-    return 1;
+        return 1;
     }
 
     int n;
     fscanf(fp, "%d\n", &n);
 
-    char palavra[20], palavraMascarada[20], letra, vet[] = "         ";
-    int i, tamanhoPalavra, erros = 0, acertos = 0, jaDigitou = 0;
+    char palavra[20], palavraMascarada[20], letra, letrasEscolhidas[] = "                        ";
+    int i, j = 1, tamanhoPalavra, erros = 0, acertos = 0;
     srand(time(NULL));
 
     (fgets(palavra, sizeof(palavra) - 1, fp));
@@ -72,46 +70,50 @@ int main(int argc, char *argv[0]) {
 
         printf("Palavra: %s\n", palavraMascarada);
         printf("Tentativas restantes: %d\n", MAX_ERROS - erros);
-        printf(" %s ", vet);
+        printf(" %s ", letrasEscolhidas);
         printf("\nDigite uma letra: ");
         scanf(" %c", &letra);
         system("cls || clear");
 
         if (!isalpha(letra)) {
-            printf("Entrada invalida! Digite uma letra.\n");
+            printf("Entrada invalida! Digite uma letra.\n\n");
             continue;
         }
 
         letra = tolower(letra);
-        jaDigitou = 0;
 
         for (i = 0; i < tamanhoPalavra; i++) {
             if (palavra[i] == letra) {
-                if (palavraMascarada[i] != '_') {
-                    printf("Voce ja digitou essa letra! Tente novamente.\n");
-                    jaDigitou = 1;
-                }
+                if (strchr(letrasEscolhidas, letra))
+                    break;
                 palavraMascarada[i] = letra;
                 acertos++;
             }
         }
 
-        if (!jaDigitou && !strchr(palavra, letra)) {
-            vet[2*erros] = letra;
-            vet[2*erros-1] = '-';
-            erros++;
+        if (!strchr(letrasEscolhidas, letra) && !strchr(palavra, letra)) {
             printf("Letra incorreta! Tente novamente.\n");
-        }
+            erros++;
+        }else if (strchr(letrasEscolhidas, letra))
+            printf("Voce ja digitou essa letra! Tente novamente.\n");
+        else
+            printf("Letra correta!\n");
 
+        if (!strchr(letrasEscolhidas, letra)) {
+            letrasEscolhidas[2 * j - 2] = letra;
+            letrasEscolhidas[2 * j - 1] = ' ';
+            j++;
+        }
         printf("\n");
     }
-    
+
     system("cls || clear");
-    if (acertos == tamanhoPalavra) {
-        printf("Voce acertou! A palavra era: %s\n", palavra);
-    } else {
-        printf("Voce perdeu! A palavra era: %s\n", palavra);
-    }
+
+    if (acertos == tamanhoPalavra)
+        printf("Voce venceu! ");
+    else
+        printf("Voce perdeu! ");
+    printf("A palavra era %s.\n", palavra);
 
     return 0;
 }
