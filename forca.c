522ac881aa79e193fe7c92e20ff64bc195a833ff
Ycaro Gabriell Borges <ycarogborges@gmail.com>
2023-05-24 08:25:17 -0300

    formatação do vscode

--- forca.c
+++ forca.c
@@ -6,7 +6,8 @@
 
 #define MAX_ERROS 6
 
-void desenho(int erros) {
+void desenho(int erros)
+{
     if (erros == 0)
         printf(" ____\n|    |\n|\n|\n|\n|\n|\n");
     else if (erros == 1)
@@ -21,11 +22,13 @@ void desenho(int erros) {
         printf(" ____\n|    |\n|    O\n|   /|\n|   / \\\n|\n|\n");
 }
 
-int main(int argc, char *argv[0]) {
-    
+int main(int argc, char *argv[0])
+{
+
     system("cls || clear");
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("Digite: %s <nomedoarquivo>\n", argv[0]);
         return 1;
     }
@@ -33,7 +36,8 @@ int main(int argc, char *argv[0]) {
     char *filename = argv[1];
     FILE *fp = fopen(filename, "r");
 
-    if (!fp) {
+    if (!fp)
+    {
         printf("Erro: não consigo abrir esse arquivo %s\n", filename);
         return 1;
     }
@@ -49,15 +53,18 @@ int main(int argc, char *argv[0]) {
 
     int aleatorio = rand() % n;
 
-    for (int i = 1; i <= aleatorio; i++) {
-        if (fgets(palavra, sizeof(palavra), fp) == NULL) {
+    for (int i = 1; i <= aleatorio; i++)
+    {
+        if (fgets(palavra, sizeof(palavra), fp) == NULL)
+        {
             printf("Erro ao ler a linha %d do arquivo!\n", aleatorio);
             return 1;
         }
     }
 
     tamanhoPalavra = strlen(palavra) - 1;
-    for (i = 0; i < tamanhoPalavra; i++) {
+    for (i = 0; i < tamanhoPalavra; i++)
+    {
         palavraMascarada[i] = '_';
     }
     palavraMascarada[i] = '\0';
@@ -65,7 +72,8 @@ int main(int argc, char *argv[0]) {
     printf("Bem-vindo ao jogo da forca!\n");
     printf("A palavra tem %d letras.\n\n", tamanhoPalavra);
 
-    while (erros < MAX_ERROS && acertos < tamanhoPalavra) {
+    while (erros < MAX_ERROS && acertos < tamanhoPalavra)
+    {
         desenho(erros);
 
         printf("Palavra: %s\n", palavraMascarada);
@@ -75,15 +83,18 @@ int main(int argc, char *argv[0]) {
         scanf(" %c", &letra);
         system("cls || clear");
 
-        if (!isalpha(letra)) {
+        if (!isalpha(letra))
+        {
             printf("Entrada invalida! Digite uma letra.\n\n");
             continue;
         }
 
         letra = tolower(letra);
 
-        for (i = 0; i < tamanhoPalavra; i++) {
-            if (palavra[i] == letra) {
+        for (i = 0; i < tamanhoPalavra; i++)
+        {
+            if (palavra[i] == letra)
+            {
                 if (strchr(letrasEscolhidas, letra))
                     break;
                 palavraMascarada[i] = letra;
@@ -91,15 +102,18 @@ int main(int argc, char *argv[0]) {
             }
         }
 
-        if (!strchr(letrasEscolhidas, letra) && !strchr(palavra, letra)) {
+        if (!strchr(letrasEscolhidas, letra) && !strchr(palavra, letra))
+        {
             printf("Letra incorreta! Tente novamente.\n");
             erros++;
-        }else if (strchr(letrasEscolhidas, letra))
+        }
+        else if (strchr(letrasEscolhidas, letra))
             printf("Voce ja digitou essa letra! Tente novamente.\n");
         else
             printf("Letra correta!\n");
 
-        if (!strchr(letrasEscolhidas, letra)) {
+        if (!strchr(letrasEscolhidas, letra))
+        {
             letrasEscolhidas[2 * j - 2] = letra;
             letrasEscolhidas[2 * j - 1] = ' ';
             j++;
